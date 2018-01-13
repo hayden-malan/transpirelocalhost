2fb22f9e27e26c2749bc5d1466e6a7610c52b828
mynameishayden <ima@bee.com>
2018-01-13 14:54:55 +1300

    linked html and css and sent to client

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require('express')
 
 var server = express()
+var bodyParser = require('body-parser')
+
+server.use(express.static('public'));
+
+server.get('/', function (req, res) {
+  res.sendFile(__dirname + '/homepage.html')
+})
 
 module.exports = server
